e2cbd11aebefa78dc29f0cfd76aee92929fe196e
Johnny <johnny860703@gmail.com>
2021-01-24 22:10:18 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 const app = express()
@@ -19,8 +21,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 // 設定首頁路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
@@ -28,7 +34,23 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('detail', { todo }))
+    .catch(error => console.error(error))
+})
 
+app.post('/todos', (req, res) => {
+  const name = req.body.name // 取資料
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 // 設定 port
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
