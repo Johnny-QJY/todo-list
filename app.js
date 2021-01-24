b89bd582e1301571bfc1f772f166a86c9b95cc2a
Johnny <johnny860703@gmail.com>
2021-01-24 17:35:16 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // 載入 express
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('doing')
