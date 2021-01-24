2b03faf33f85d49aed5284b0c0301cb995db53c0
Johnny <johnny860703@gmail.com>
2021-01-24 23:32:36 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
@@ -45,11 +46,40 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.error(error))
 })
 
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.error(error))
+})
+
+
 app.post('/todos', (req, res) => {
   const name = req.body.name // 取資料
   return Todo.create({ name })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
 })
 // 設定 port
 app.listen(port, () => {
