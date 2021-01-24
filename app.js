1e6bc3690eedc8bd8bc08dacfe17eed660e29187
Johnny <johnny860703@gmail.com>
2021-01-24 20:03:14 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // 載入 express
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
 const port = 3000
-
+// 資料庫設定
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
@@ -17,9 +17,12 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('doing')
+  res.render('index')
 })
 
 // 設定 port
